7da76338fae99ff5eb3df2a50d14f34c5d37ff07
Mike Melanson <mike@multimedia.cx>
2015-04-11 12:57:41 -0700

    Initial commit: the player API header file
    This header file defines the interface that all the Cirrus Retro
    players will export.

---
+++ player_interface.h
@@ -0,0 +1,18 @@
+#ifndef PLAYER_INTERFACE
+#define PLAYER_INTERFACE
+
+extern "C"
+{
+    int crPlayerContextSize();
+    int crPlayerInitialize(void *context, int sampleRate);
+    int crPlayerLoadFile(void *context, const char *filename, unsigned char *data, int size);
+    int crPlayerSetTrack(void *context, int track);
+    int crPlayerGenerateStereoFrames(void *context, int16_t *samples, int frameCount);
+    int crPlayerVoicesCanBeToggled(void *context);
+    int crPlayerGetVoiceCount(void *context);
+    char *crPlayerGetVoiceName(void *context, int voice);
+    void crPlayerSetVoiceState(void *context, int voice, int enabled);
+    void crPlayerCleanup(void *context);
+};
+
+#endif  // PLAYER_INTERFACE
