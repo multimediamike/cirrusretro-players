6e62233da7a8aeaad4fe1354bd6f50ca8e2258ef
Mike Melanson <mike@multimedia.cx>
2015-04-15 22:29:27 -0700

    Refactor XZ decoding function
    This change refactors the XZ decoding glue function so that it
    decompresses the entire buffer in one run. This requires that the
    calling function pass in a buffer that can hold the entire output.

--- xzdec.c
+++ xzdec.c
@@ -1,18 +1,15 @@
-#include <stdlib.h>
 #include "xz.h"
 
-#define BUFFER_INCREMENT (1024 * 1024 * 10)
-
 int xz_decompress(unsigned char *encoded, int encoded_size,
-    unsigned char **decoded, int *decoded_size)
+    unsigned char *decoded, int decoded_size)
 {
     static int xz_initialized = 0;
     enum xz_ret ret;
     struct xz_dec *xz;
     struct xz_buf buf;
-    unsigned char *dec_buffer;
-    unsigned char *temp;
-    int dec_size;
+
+    if (!encoded || !decoded)
+        return 0;
 
     if (!xz_initialized)
     {
@@ -20,77 +17,23 @@ int xz_decompress(unsigned char *encoded, int encoded_size,
         xz_initialized = 1;
     }
 
-    *decoded = NULL;
-    *decoded_size = 0;
-
     buf.in = encoded;
     buf.in_pos = 0;
     buf.in_size = encoded_size;
 
-    dec_size = BUFFER_INCREMENT;
-    dec_buffer = (unsigned char *)malloc(dec_size);
-    if (!dec_buffer)
-    {
-        return 0;
-    }
-
-    buf.out = dec_buffer;
+    buf.out = decoded;
     buf.out_pos = 0;
-    buf.out_size = dec_size;
+    buf.out_size = decoded_size;
 
-    xz = xz_dec_init(XZ_DYNALLOC, (uint32_t)-1);
+    xz = xz_dec_init(XZ_SINGLE, 0);
     if (!xz)
     {
-        free(dec_buffer);
         return 0;
     }
 
-    do
-    {
-        ret = xz_dec_run(xz, &buf);
-        if (ret == XZ_OK)
-        {
-            /* things are okay, but more buffer space is needed */
-            dec_size += BUFFER_INCREMENT;
-            temp = realloc(dec_buffer, dec_size);
-            if (!temp)
-            {
-                free(dec_buffer);
-                xz_dec_end(xz);
-                return 0;
-            }
-            else
-                dec_buffer = temp;
-            buf.out_size = dec_size;
-            buf.out = dec_buffer;
-        }
-        else
-        {
-            /* any other status is an exit condition (either error or stream end) */
-            break;
-        }
-    } while (ret != XZ_STREAM_END);
-
-    if (ret == XZ_STREAM_END)
-    {
-        /* resize the final buffer */
-        dec_size = buf.out_pos;
-        temp = realloc(dec_buffer, dec_size);
-        if (!temp)
-        {
-            free(dec_buffer);
-            xz_dec_end(xz);
-            return 0;
-        }
-        *decoded = temp;
-        *decoded_size = dec_size;
-        xz_dec_end(xz);
-        return 1;
-    }
+    ret = xz_dec_run(xz, &buf);
+    if (ret != XZ_STREAM_END)
+        return 0;
     else
-    {
-        xz_dec_end(xz);
-        free(dec_buffer);
-        return 0;
-    }
+        return 1;
 }
--- xzdec.h
+++ xzdec.h
@@ -2,6 +2,6 @@
 #define _XZDEC_H_
 
 int xz_decompress(unsigned char *encoded, int encoded_size,
-  unsigned char **decoded, int *decoded_size);
+    unsigned char *decoded, int decoded_size);
 
 #endif  // _XZDEC_H_
