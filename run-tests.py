f818470959ec17799b8017ddda7c1dc5dbd1a61d
Mike Melanson <mike@multimedia.cx>
2015-05-02 21:26:41 -0700

    Initial commit for a script that runs all tests

---
+++ run-tests.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import commands
+import glob
+import json
+import multiprocessing
+
+def runTest(spec):
+    command = "NODE_PATH=$PWD/final /usr/bin/nodejs cr-test-harness.js " + spec
+    (status, output) = commands.getstatusoutput(command)
+    return { 'spec': spec, 'status': status, 'output': output }
+
+if __name__ == "__main__":
+    testSpecs = glob.glob("test-specs/*.json")
+    pool = multiprocessing.Pool()
+    it = pool.imap_unordered(runTest, testSpecs);
+    succeeded = 0
+    try:
+        while 1:
+            result = it.next()
+            if result['status'] == 0:
+                succeeded += 1
+                print "."
+            else:
+                print result['spec'] + " failed:" + result['output']
+    except StopIteration:
+        pass
+
+    print "%d / %d tests succeeded" % (succeeded, len(testSpecs));
