71815b1d08dccabf90e17bd32fe3ec63e51f95e9
Mike Melanson <mike@multimedia.cx>
2015-05-06 18:10:19 -0700

    Document xzdec.h and make it callable from C++
    Add at least a minimal amount of documentation for the xz_decompress()
    workhorse function. Also, make sure that C++ can call it, even though
    it's built with the C compiler.

--- xzdec.h
+++ xzdec.h
@@ -1,7 +1,21 @@
 #ifndef _XZDEC_H_
 #define _XZDEC_H_
 
-int xz_decompress(unsigned char *encoded, int encoded_size,
-    unsigned char *decoded, int decoded_size);
+extern "C"
+{
+    /* Decompress a buffer that is compressed with the XZ algorithm.
+     * Note that this leverages the XZ-embedded library which can only
+     * handle CRC32 integrity checks, e.g., data that was encoded with
+     * 'xz --check=crc32 ...' which is NOT the default for xz.
+     *
+     * Size of decompressed data needs to be known in advance since this
+     * function expects that the 'decoded' buffer will point to enough
+     * space to hold all the data.
+     *
+     * Returns non-zero on success, 0 on failure.
+     */
+    int xz_decompress(unsigned char *encoded, int encoded_size,
+        unsigned char *decoded, int decoded_size);
+};
 
 #endif  // _XZDEC_H_
